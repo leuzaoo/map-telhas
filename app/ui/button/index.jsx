58663b91302054f5b5202e5db21624eadbff1aa6
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-04-25 20:57:37 -0300

    Created button component

---
+++ app/ui/button/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function MyButton({ text, style }) {
+  let buttonClassName =
+    "mt-5 py-3 px-5 uppercase text-xl lg:text-3xl rounded-xl font-bold transition-all duration-200  hover:opacity-80";
+
+  switch (style) {
+    case "primary":
+      buttonClassName += " bg-white text-primaryRed";
+      break;
+    case "white":
+      buttonClassName += " bg-transparent";
+      break;
+    case "red-white":
+      buttonClassName += " bg-primaryRed text-white";
+      break;
+    case "dark-white":
+      buttonClassName += " bg-strongDark text-white";
+      break;
+    default:
+      // Se nenhum estilo correspondente for encontrado, aplicar estilo padrão
+      buttonClassName += " bg-primaryRed hover:opacity-80";
+  }
+  return (
+    <button className={buttonClassName}>
+      <span>{text}</span>
+    </button>
+  );
+}
