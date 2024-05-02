2296daad64805e28ed8ed97c4ba93caa86a54e9b
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-05-02 16:34:17 -0300

    Updated infos

--- app/data/produtos.js
+++ app/data/produtos.js
@@ -5,11 +5,15 @@ export const produtos = [
     name: "Telha Simples",
     model: "Trapézio 40/980",
     description:
-      "Telha metálica simples, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura.",
+      "Telha metálica simples, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura. Nossas telhas galvalume passam por um rigoroso processo de perfilação. Feitas com uma chapa revestida de alumínio, zinco e silício, garantem alta resistência à corrosão, combinando durabilidade e proteção. Com essa composição, têm uma vida útil de até 50 anos.",
     material: "Galvanizado, Galvalume, Colorido.",
     application:
       "Ideal para coberturas industriais, comerciais e residenciais.",
     image: "/original/telha-simples.png",
+    maintenance:
+      "Na manutenção de telhados metálicos, procedimentos incluem identificação da avaria, limpeza da área afetada, remoção de detritos ou ferrugem, restauração ou substituição da peça danificada, conforme necessário.",
+    personalization:
+      "Além de proporcionar maior segurança e durabilidade, as telhas podem receber pintura eletrostática, mantendo sua aparência preservada ao longo do tempo e contribuindo para a valorização do imóvel. Esse processo moderno e avançado garante proteção e alto acabamento, utilizando cargas elétricas para fixação da tinta, resultando em durabilidade de mais de cinco anos, com acabamento espelhado, liso e sem falhas.",
   },
   {
     id: 2,
@@ -17,11 +21,15 @@ export const produtos = [
     name: "Telha Sanduíche (Termoacústica)",
     model: "Trapézio 40/980",
     description:
-      "Telha metálica termoacústica, composta por duas chapas metálicas e núcleo isolante, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura.",
+      "Telha metálica termoacústica, composta por duas chapas metálicas e núcleo isolante, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura. Além de proporcionar isolamento acústico, as telhas sanduíche oferecem uma redução térmica de 40% para fechamentos e coberturas, controlando o superaquecimento e evitando condensação e gotejamento de água.",
     material: "Galvanizado, Galvalume, Colorido.",
     application:
       "Indicadas para ambientes que exigem isolamento térmico e acústico, como galpões industriais, câmaras frias, entre outros.",
     image: "/original/telha-sanduiche.png",
+    maintenance:
+      "Na manutenção de telhados metálicos, são realizados procedimentos como identificação da avaria, limpeza da superfície afetada, remoção de detritos ou ferrugem, restauração da área danificada e, se necessário, substituição da peça danificada.",
+    personalization:
+      "Além, de maior segurança, isolamento e durabilidade, ela pode receber a pintura eletrostática, mantendo a aparência ao longo do tempo preservada e contribuindo para valorização do imóvel. A pintura eletrostática é um dos processos mais modernos e avançados de pinturas e revestimentos para peças que necessitam de proteção e um alto nível de acabamento, seu processo diferenciado por meio de cargas elétricas para a fixação da tinta garantem a durabilidade de mais de cinco anos, deixando o acabamento espelhado, sem bolhas ou gotejamento, liso e sem manchas.",
   },
   {
     id: 3,
@@ -29,11 +37,15 @@ export const produtos = [
     name: "Telha Semi Sanduíche (Termoacústica)",
     model: "Trapézio 40/980",
     description:
-      "Similar às telhas sanduíches, porém com um lado da telha sem o núcleo isolante, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura.",
+      "Similar às telhas sanduíches, porém com um lado da telha sem o núcleo isolante, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura. Nossas telhas semi-sanduíche passam por um rigoroso processo de perfilação. Feitas com chapas de aço galvalume revestidas de alumínio, zinco e silício, garantem alta resistência à corrosão, combinando durabilidade e proteção. Com essa composição, têm vida útil de até 50 anos e um núcleo isolante abaixo da chapa que reduz o risco de propagação de chamas, umidade e melhora o isolamento acústico.",
     material: "Galvanizado, Galvalume, Colorido.",
     application:
       "Utilizadas em ambientes que necessitam de isolamento térmico parcial, como coberturas em que somente um lado precisa de isolamento.",
     image: "/original/telha-semi-sanduiche.png",
+    maintenance:
+      "Na manutenção dos telhados metálicos, são executados processos como identificação e análise do tipo de dano presente, limpeza da área a ser reparada, remoção de resíduos ou corrosão, restauração da região afetada e, ocasionalmente, substituição da peça danificada.",
+    personalization:
+      "Além de oferecer maior segurança, isolamento e durabilidade, a telha pode ser submetida a pintura eletrostática, mantendo sua aparência ao longo do tempo e contribuindo para a valorização do imóvel. A pintura eletrostática é um dos métodos mais avançados e modernos de revestimento para peças que demandam proteção e alto padrão estético. Esse processo, que utiliza cargas elétricas para fixar a tinta, assegura uma durabilidade superior a cinco anos, resultando em um acabamento espelhado, livre de bolhas, gotejamento, liso e sem manchas.",
   },
   {
     id: 4,
@@ -41,11 +53,15 @@ export const produtos = [
     name: "Telha Sanduíche (Termoacústica) Tipo Forro",
     model: "Trapézio 40/980",
     description:
-      "Telha metálica termoacústica específica para uso como forro, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura.",
+      "Telha metálica termoacústica específica para uso como forro, de perfil trapézio, com medidas de 40mm de altura e 980mm de largura. O tipo de telha forro oferece um acabamento interno para o ambiente, eliminando a necessidade de instalar um forro adicional no teto, já que a própria telha realiza essa função. Além disso, devido à sua estrutura sanduíche, proporciona isolamento termoacústico, resultando em uma redução térmica de 40% para fechamentos e coberturas. Esse controle térmico ajuda a evitar o superaquecimento das coberturas, prevenindo a condensação e o gotejamento de água.",
     material: "Galvanizado, Galvalume, Colorido.",
     application:
       "Indicadas para forros em ambientes industriais, comerciais e residenciais.",
     image: "/original/telha-sanduiche-tipo-forro.png",
+    maintenance:
+      "Durante a manutenção dos telhados metálicos, são efetuados processos como identificação e avaliação do tipo de dano presente, limpeza da superfície a ser reparada, remoção de detritos ou oxidação, restauração da região afetada e, ocasionalmente, substituição da peça danificada.",
+    personalization:
+      "Além de proporcionar maior segurança, isolamento e durabilidade, a telha pode ser revestida com pintura eletrostática, mantendo sua aparência ao longo do tempo e contribuindo para valorizar o imóvel. A pintura eletrostática é um dos métodos mais avançados e modernos de revestimento, especialmente para peças que requerem proteção e acabamento refinado. Esse processo exclusivo, que utiliza cargas elétricas para fixar a tinta, assegura uma durabilidade superior a cinco anos, resultando em um acabamento espelhado, livre de bolhas, gotejamento, liso e sem manchas.",
   },
   {
     id: 5,
@@ -53,11 +69,15 @@ export const produtos = [
     name: "Telha Translúcida",
     model: "Trapézio 40/980",
     description:
-      "Telha em material translúcido, que permite a passagem de luz natural.",
+      "Telha em material translúcido, que permite a passagem de luz natural. Apesar de serem transparentes, as telhas translúcidas são projetadas para serem duráveis e resistentes, capazes de suportar condições climáticas adversas, como vento, chuva, granizo e radiação ultravioleta. Além de sua função prática, as telhas translúcidas adicionam um aspecto estético interessante ao ambiente, proporcionando uma sensação de leveza e luminosidade.",
     material: "",
     application:
       "Utilizadas em conjunto com telhas metálicas para proporcionar iluminação natural em ambientes fechados.",
     image: "/original/telha-translucida.png",
+    maintenance:
+      "É fundamental manter as telhas limpas para garantir a transmissão adequada de luz. Limpe-as periodicamente com água e detergente neutro, evitando o uso de produtos abrasivos que possam danificar a superfície. Tome cuidado ao caminhar sobre as telhas translúcidas para evitar danos causados por impactos. Use calçados adequados e evite colocar objetos pesados sobre elas. Faça inspeções periódicas para identificar possíveis danos, como rachaduras, trincas ou acúmulo de sujeira que possam afetar a transmissão de luz ou comprometer a integridade da telha.",
+    personalization:
+      "As telhas translúcidas estão disponíveis em uma variedade de cores e tonalidades, permitindo que você escolha uma opção que se adapte ao estilo do seu ambiente ou que proporcione a quantidade desejada de luz filtrada.",
   },
   {
     id: 6,
@@ -65,9 +85,28 @@ export const produtos = [
     name: "Cumeeira",
     model: "",
     description:
-      "Peça utilizada para fazer o acabamento no topo das telhas, proporcionando vedação e proteção contra interpéries.",
+      "Peça utilizada para fazer o acabamento no topo das telhas, proporcionando vedação e proteção contra interpéries. As chapas usadas são revestidas com alumínio, zinco e silício, garantindo resistência à corrosão. A telha galvalume, devido à sua composição, é altamente resistente à corrosão e durável, com vida útil de até 50 anos.",
     material: "Galvanizado, Galvalume, Colorido.",
     application: "",
     image: "/original/cumeeira.png",
+    maintenance:
+      "Enquanto os telhados metálicos são mantidos, são feitos processos como detecção e classificação do dano na área, higienização da superfície a ser consertada, retirada de detritos ou oxidação, recuperação da região afetada ou, em algumas situações, troca da peça danificada.",
+    personalization:
+      "Além, de maior segurança e durabilidade, ela pode receber a pintura eletrostática, mantendo a aparência ao longo do tempo preservada e contribuindo para valorização do imóvel. A pintura eletrostática é um dos processos mais modernos e avançados de pinturas e revestimentos para peças que necessitam de proteção e um alto nível de acabamento, seu processo diferenciado por meio de cargas elétricas para a fixação da tinta garantem a durabilidade de mais de cinco anos, deixando o acabamento espelhado, sem bolhas ou gotejamento, liso e sem manchas.",
+  },
+  {
+    id: 7,
+    slug: "pintura-eletrostatica",
+    name: "Pintura Eletrostática",
+    model: "",
+    description:
+      "A pintura eletrostática é um processo que cria uma camada protetora, utilizando a aplicação de um pó pigmentado eletricamente carregado. Esse pó é fixado na superfície do material através de atração e aquecido posteriormente para formar uma camada de tinta sólida. Essa técnica oferece vantagens como resistência e durabilidade, proteção contra corrosão e oxidação, e é ecologicamente correta, pois não utiliza solventes tóxicos e permite o reaproveitamento do excesso de material.",
+    material: "",
+    application:
+      "Personalização estética das telhas conforme preferência do cliente.",
+    image: "/original/pintura-eletrostatica.png",
+    maintenance:
+      "Faça uma limpeza regular das superfícies pintadas com pintura eletrostática para remover poeira, sujeira e outros resíduos. Use água morna e um detergente suave, evitando produtos abrasivos que possam danificar a pintura. Faça inspeções visuais periódicas para verificar se há sinais de desgaste, descamação, corrosão ou danos na pintura. Se encontrar alguma área comprometida, programe reparos imediatamente para evitar danos adicionais.",
+    personalization: "",
   },
 ];
