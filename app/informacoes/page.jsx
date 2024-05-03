ffd4f664dbac3b5fd0068f6c2062f8bb2960ed37
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-05-03 16:35:04 -0300

    Created infos page

---
+++ app/informacoes/page.jsx
@@ -0,0 +1,12 @@
+import Center from "../ui/center";
+
+export default function InformationPage() {
+  return (
+    <div className="py-5 px-5 text-xl font-medium">
+      <Center>
+        <h1 className="uppercase">Informações</h1>
+        <hr />
+      </Center>
+    </div>
+  );
+}
