9c9758041901f4769cb4185eb16a7c22bd95c115
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-05-04 16:08:59 -0300

    Updated desktop version

--- app/sobre/page.jsx
+++ app/sobre/page.jsx
@@ -16,79 +16,81 @@ export default function AboutPage() {
             </h1>
             <div className="my-3 h-[2px] w-5/6 bg-primaryRed ml-auto" />
           </div>
-          <Image
-            className="rounded-xl shadow-md"
-            src="/original/about-us-2.jpg"
-            width={1500}
-            height={1000}
-            alt="Sobre nós"
-          />
-          <div className="my-5 flex flex-col gap-3 text-lg ">
-            <p>
-              Nossa gama de produtos inclui uma variedade de telhas metálicas,
-              cumeeiras, bobinas slitadas e oferecemos serviços de pintura
-              eletrostática para atender as necessidades específicas de nossos
-              clientes.
-            </p>
-            <p>
-              Na Map Telhas, a qualidade é a nossa prioridade número um.
-              Contamos com uma equipe dedicada de profissionais altamente
-              qualificados e experientes, comprometidos em oferecer produtos e
-              serviços que superem as expectativas de nossos clientes.
-            </p>
-            <p>
-              Desde <span className="font-bold">1996</span>, nos dedicamos
-              incansavelmente a fornecer telhas telhas metálicas de alta
-              qualidade e serviços relacionados para uma ampla gama de
-              aplicações. Trabalhamos com os melhores aços disponíveis no
-              mercado, garantindo durabilidade, resistência e desempenho
-              superiores em cada projeto que realizamos.
-            </p>
-            <Image
-              className="rounded-xl shadow-md"
-              src="/original/about-us-3.png"
-              width={1000}
-              height={1000}
-              alt="Sobre nós"
-            />
+          <div className="lg:grid grid-cols-2 text-lg gap-10">
+            <div className="my-5 flex flex-col gap-3 lg:text-justify">
+              <Image
+                className="rounded-xl shadow-md"
+                src="/original/about-us-2.jpg"
+                width={1500}
+                height={1000}
+                alt="Sobre nós"
+              />
+              <p>
+                Nossa gama de produtos inclui uma variedade de telhas metálicas,
+                cumeeiras, bobinas slitadas e oferecemos serviços de pintura
+                eletrostática para atender as necessidades específicas de nossos
+                clientes.
+              </p>
+              <p>
+                Na Map Telhas, a qualidade é a nossa prioridade número um.
+                Contamos com uma equipe dedicada de profissionais altamente
+                qualificados e experientes, comprometidos em oferecer produtos e
+                serviços que superem as expectativas de nossos clientes.
+              </p>
+            </div>
+            <div className="lg:text-justify">
+              <div className="hidden lg:contents">
+                <p className="hidden">Sobre nós</p>
+                <p className="uppercase text-primaryRed text-[40px] font-extrabold text-right">
+                  A Empresa
+                </p>
+                <p className="text-right text-3xl">
+                  Localizada em Campo Limpo Paulista/SP
+                </p>
+                <div className="my-3 h-[2px] w-5/6 bg-primaryRed ml-auto" />
+              </div>
+              <p>
+                Desde <span className="font-bold">1996</span>, nos dedicamos
+                incansavelmente a fornecer telhas telhas metálicas de alta
+                qualidade e serviços relacionados para uma ampla gama de
+                aplicações. Trabalhamos com os melhores aços disponíveis no
+                mercado, garantindo durabilidade, resistência e desempenho
+                superiores em cada projeto que realizamos.
+              </p>
+              <div>
+                <Image
+                  className="rounded-xl shadow-md mt-5"
+                  src="/original/about-us-3.png"
+                  width={1000}
+                  height={1000}
+                  alt="Sobre nós"
+                />
+              </div>
+            </div>
           </div>
         </section>
-        <section>
+        <section className="mt-5">
           <h1 className="uppercase text-primaryRed text-3xl font-extrabold text-center">
             Junte-se a Nós!
           </h1>
-          <div className="mt-5 pb-5 flex items-center gap-3 overflow-x-auto">
+          <div className="mt-5 pb-5 flex items-center gap-3 lg:gap-0 justify-between overflow-x-auto">
             <Image
-              className="rounded-xl shadow-md max-h-40"
+              className="rounded-xl shadow-md max-w-80"
               src="/original/carrousel-1.png"
               width={2000}
               height={2000}
               alt="Sobre nós"
             />
             <Image
-              className="rounded-xl shadow-md max-h-40"
+              className="rounded-xl shadow-md max-w-80"
               src="/original/carrousel-2.png"
               width={2000}
               height={2000}
               alt="Sobre nós"
             />
             <Image
-              className="rounded-xl shadow-md max-h-40"
+              className="rounded-xl shadow-md max-w-80"
               src="/original/carrousel-3.png"
-              width={2000}
-              height={2000}
-              alt="Sobre nós"
-            />
-            <Image
-              className="rounded-xl shadow-md max-h-40"
-              src="/original/carrousel-4.png"
-              width={2000}
-              height={2000}
-              alt="Sobre nós"
-            />
-            <Image
-              className="rounded-xl shadow-md max-h-40"
-              src="/original/carrousel-5.png"
               width={2000}
               height={2000}
               alt="Sobre nós"
