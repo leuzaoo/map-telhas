14426db299904639aa33f8027890139f9c4fe6c6
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-05-07 13:23:19 -0300

    Created new branch and contact page.

---
+++ app/contato/page.jsx
@@ -0,0 +1,14 @@
+import Center from "../ui/center";
+
+export default function ContactPage() {
+  return (
+    <div className="py-5">
+      <Center>
+        <h1 className="text-title uppercase font-extrabold text-primaryRed">
+          Entre em Contato
+        </h1>
+        <div className="h-[2px] w-5/6 bg-primaryRed" />
+      </Center>
+    </div>
+  );
+}
