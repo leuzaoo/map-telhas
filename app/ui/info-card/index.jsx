76600914c8969b95a67a8fb43ed96886315199cf
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-05-10 10:58:42 -0300

    Updated card info

--- app/ui/info-card/index.jsx
+++ app/ui/info-card/index.jsx
@@ -5,12 +5,13 @@ export default function InfoCard({ title, desc, image }) {
   return (
     <div className="flex flex-col justify-between min-w-xs max-w-xs min-h-[340px] max-h-[340px] bg-neutral-50 rounded-xl shadow-md">
       <Image
-        className="rounded-xl"
+        className="rounded-xl max-h-44 object-cover"
         src={image}
         alt="Image"
         width={1000}
         height={1000}
       />
+
       <div className="p-3">
         <h2 className="text-center">{title}</h2>
         <h3 className="text-sm">{desc}</h3>
