d8eccc4d0f38ad5233cd9149d2c3734da4fbf959
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-06-04 10:47:47 -0300

    Updated SEO

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "MAP Telhas Metálicas e Pintura Eletrostática.",
+  title: "MAP Telhas Metálicas e Pintura Eletrostática",
   description:
     "Somos uma empresa especializada na fabricação de telhas metálicas e pintura eletrostática desde 1996. Localizada em Campo Limpo Paulista.",
 };
