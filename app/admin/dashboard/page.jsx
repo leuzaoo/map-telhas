b0453f781eeec07b048b0556c8a8fdc415de6d84
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-06-06 11:02:29 -0300

    Created new pages

---
+++ app/admin/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import MessagesInfo from "../../ui/messages-info";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <MessagesInfo />
+    </div>
+  );
+}
---
+++ app/admin/page.jsx
@@ -0,0 +1,9 @@
+import AdminLayout from "../ui/adminPage/AdminLayout";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <AdminLayout />
+    </div>
+  );
+}
