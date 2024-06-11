97cfeeba55de4a754d3646acdc0d21a5c7cc2d89
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-06-11 14:54:56 -0300

    updates

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,13 +1,20 @@
+// lib/dbConnect.js
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
   try {
-    if (mongoose.connection.readyState === 0) {
-      await mongoose.connect(process.env.MONGODB_URI);
-      console.log("Conectado ao banco de dados.");
-    }
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Conectado ao banco de dados.");
   } catch (error) {
-    console.log("Erro ao tentar conectar ao banco de dados: ", error);
+    console.error("Erro ao tentar conectar ao banco de dados: ", error);
   }
 };
 
