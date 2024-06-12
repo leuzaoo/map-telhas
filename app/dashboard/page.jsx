38c53e5021832f28cdf466c783b8787ed7600ab0
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-06-12 09:38:39 -0300

    Updated dashboard page to show message count

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import { fetchMessages } from "../api/mensagens/route";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const { messages, messageCount } = await fetchMessages();
+
   return (
-    <div className="text-2xl font-bold grid mx-auto h-screen w-full place-items-center">
-      Em manutenção.
-    </div>
+    <>
+      <div className="bg-blue-100 border border-blue-300 rounded-md shadow-lg p-4 mb-4">
+        <h2 className="text-lg font-semibold">Total de Mensagens</h2>
+        <p className="text-2xl font-bold">{messageCount}</p>
+      </div>
+      <div className="text-2xl font-bold grid mx-auto h-screen w-full place-items-center">
+        Em manutenção.
+      </div>
+    </>
   );
 }
