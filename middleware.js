774d80d298c0e63ada6683b6ca31e34a558e3ab4
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-06-15 01:26:20 -0300

    Updated middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,20 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export const config = { matcher: ["/dashboard"] };
+export async function middleware(req) {
+  const url = req.nextUrl.clone();
+  const token = await getToken({ req });
+
+  if (token && url.pathname === "/admin") {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin", "/dashboard/:path*"],
+};
