443b2236f014419158cd7df570f25588a4840727
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-06-17 14:16:55 -0300

    Add google tag manager and facebook pixel

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "./ui/navbar";
 import Footer from "./ui/footer";
 import Head from "next/head";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,8 +24,51 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MZ977R2Z');
+          `}
+      </Script>
       <html lang="pt">
         <body id="Telhas Metálicas" className={`${poppins.className}`}>
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1155733015746543');
+            fbq('track', 'PageView');
+          `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style="display:none"
+              src="https://www.facebook.com/tr?id=1155733015746543&ev=PageView&noscript=1"
+            />
+          </noscript>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MZ977R2Z"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <h1 className="hidden">
             Map Telhas Metálicas e Pintura Eletrostática
           </h1>
