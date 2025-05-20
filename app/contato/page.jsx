da9cf2e11c7355b372130bd5a8ae923a0264fcc7
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-05-20 11:15:50 -0300

    Fixed google class name error

--- app/contato/page.jsx
+++ app/contato/page.jsx
@@ -37,7 +37,7 @@ const List = [
           className="w-full min-h-[400px] rounded-xl"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29337.180465741003!2d-46.832185125683644!3d-23.20129249999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cedf228749fb63%3A0x926d241a8c4754d2!2sMap%20Calhas%20e%20Telhas!5e0!3m2!1spt-BR!2sbr!4v1715182976809!5m2!1spt-BR!2sbr"
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         />
         <Link
           target="_blank"
